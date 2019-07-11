1ddf152bfd75105e60a45f6022dceb69c3a363ca
Michal Swigost <mike@data.pl>
2019-07-11 15:47:08 +0200

    add mustache

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+/************* MUSTACHE pętla *****************/
+var listItems = '';
+var templateItem = document.getElementById('template-product-item').innerHTML;
+//Mustache.parse(templateItem);
+
+for(var i = 0; i < carouselData.length; i++){
+		//console.log(carouselData[i]);
+		listItems += Mustache.render(templateItem, carouselData[i]);
+    
+	};
+
+//var fullList = Mustache.render(listItems);
+
+results.insertAdjacentHTML('beforeend', listItems);
+
+
+
 /************ Karuzela **********/
 var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity( elem, {
